2590ff2ec65c0a345105b9cef1e08af3d039bd59
aaron777collins <aaron777collins@gmail.com>
2022-12-15 20:44:16 -0500

    styles

--- ImmersionInTheNeighbourhood.js
+++ ImmersionInTheNeighbourhood.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // highlighting the current tab
 
     let tabs = document.getElementsByClassName("subnavtab");
+    let title = document.getElementsByClassName("subnavtitle");
 
     for (let i = 0; i < tabs.length; i++) {
 
@@ -16,5 +17,9 @@ document.addEventListener('DOMContentLoaded', function() {
         tabs.item(i).addEventListener("mouseout", (e) => {
             e.target.style["text-decoration"] = "none";
         });*/
+    }
+
+    if (title.item(0).getAttribute("href").split("/").pop() === window.location.pathname.split("/").pop()) {
+        title.item(0).style["text-decoration-color"] = "rgba(0,0,0,1)";
     }
 }, false);
